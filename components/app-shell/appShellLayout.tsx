85cf463f63464add70dbd3cce78e06cc04bf2cf8
Gepiro <gepiro0916@gmail.com>
2024-06-17 10:26:29 +0200

    Changed header

--- components/app-shell/appShellLayout.tsx
+++ components/app-shell/appShellLayout.tsx
@@ -3,6 +3,8 @@ import { AppShell, Burger, Group, Title } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import classes from "./appShellLayout.module.css";
 import Link from "next/link";
+import logo from "@/assets/logo.png";
+import Image from "next/image";
 
 export function AppShellLayout({ children }: { children: React.ReactNode }) {
   const [opened, { toggle }] = useDisclosure();
@@ -14,7 +16,7 @@ export function AppShellLayout({ children }: { children: React.ReactNode }) {
         navbar: { backgroundColor: "#242b33" },
         main: { backgroundColor: "#c1dae7" },
       }}
-      header={{ height: 60 }}
+      header={{ height: 90 }}
       navbar={{
         width: 300,
         breakpoint: "sm",
@@ -23,29 +25,26 @@ export function AppShellLayout({ children }: { children: React.ReactNode }) {
       padding="md"
     >
       <AppShell.Header>
-        <Group h="100%" px="md">
-          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+        <Group h="100%" px="md" style={{marginLeft: "15%", marginRight: "15%"}}>
+          <Burger opened={opened} onClick={toggle} hiddenFrom="md" size="sm" />
           <Group justify="space-between" style={{ flex: 1 }}>
-            <Title style={{ color: "#242b33" }}>Trustalert</Title>
-            <Group ml="xl" gap={0} visibleFrom="sm">
+            <Title style={{ color: "#03a8f4" }} order={5}>Trust Alert</Title>
+			<Group justify="space-between" visibleFrom="md">
+			<Title style={{ color: "#12688d" }} order={4} ta="center">TrustAlert is a project <br/> funded by</Title>
+			<Image src={logo} alt="logo" />
+			</Group>
+            <Group ml="xl" gap={0} visibleFrom="md">
               <Link className={classes.link_desktop} href={"/"}>
                 Home
               </Link>
-              {/* <Link className={classes.link_desktop} href={"/"}>
-                Home
-              </Link> */}
             </Group>
           </Group>
         </Group>
       </AppShell.Header>
-
       <AppShell.Navbar py="lg" px={4}>
         <Link className={classes.link} href={"/"}>
           <span>Home</span>
         </Link>
-        {/* <Link className={classes.link} href={"/"}>
-          <span>Home</span>
-        </Link> */}
       </AppShell.Navbar>
 
       <AppShell.Main>{children}</AppShell.Main>
