8193d88f91257e663d5c3bfa124d3bbd56573682
Gepiro <gepiro0916@gmail.com>
2024-08-20 13:36:46 +0200

    Fixed dockerfile download_server

--- download_server/server.js
+++ download_server/server.js
@@ -1,24 +1,20 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-// const bodyParser = require("body-parser");
-var http = require("http");
 var https = require("https");
 var fs = require("fs");
+
 var privateKey = fs.readFileSync("sslcert/server.key", "utf8");
 var certificate = fs.readFileSync("sslcert/server.crt", "utf8");
 
 var credentials = { key: privateKey, cert: certificate };
 var app = express();
 
-var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 
-httpServer.listen(8080);
 httpsServer.listen(8443);
 
 app.use(cors());
-// app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   console.log("Server running");
@@ -28,10 +24,6 @@ app.get("/", (req, res) => {
 app.get("/download/:timestamp", (req, res) => {
   const { timestamp } = req.params;
   const filePath = path.join("storage", timestamp, "prediction.csv");
-  console.log("Apro questo file: " + filePath);
-  res.download(filePath); 
-});
-
-app.listen(3001, () => {
-  console.log(`Download server running on port 3001`);
+  console.log(`[${timestamp}] Open file: ${filePath}`);
+  res.download(filePath);
 });
